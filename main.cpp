605a358553518b9da0fe84b2b113fd1ece25f523
Julio Berina <julioberina@gmail.com>
2017-05-31 23:50:59 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double f1(double x, double y)
+{
+	return ((1.0 / 4.0)*pow(x, 4) - (1.0 / 3.0)*pow(x, 3));
+}
+
+int main()
+{
+
+	return 0;
+}
